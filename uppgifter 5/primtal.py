ebaa839080a94ef65b30ec1541de5a4a70a1cc74
Mark Taratynov <mark.taratynov@gmail.com>
2024-02-01 14:14:59 +0100

    primtal

--- uppgifter 5/primtal.py
+++ uppgifter 5/primtal.py
@@ -1,4 +1,16 @@
-primtal = input(f"Ange primtal...")
-primtalNummer = int(primtal)
+def arPrimTal(num):
+    if num <= 1:
+        return False
+    for i in range(2, num):
+        if num % i == 0:
+            return False
+    return True
 
-print(f"Ditt primtal är: ", primtal)
+try:
+    primtal = int(input("Ange primtal..."))
+    print("Ditt primtal mellan 1 och", primtal, ":")
+    for num in range(2, primtal + 1):
+        if arPrimTal(num):
+            print(num, end = " ")
+except ValueError:
+    print("Du maste ange ett heltal.")
